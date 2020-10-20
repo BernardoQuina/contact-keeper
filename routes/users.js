8751465eabf3b4fe46d9886bae338a03cabd1bfb
Bernardo-Quina <bernardoquina@gmail.com>
2020-10-20 16:03:06 +0100

    Authenticate Route

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,8 @@ router.post('/', [
   check('name', 'Please add name').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
-], async (req, res) => {
+],
+async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
